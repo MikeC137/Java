4aa7b1bff72a8613362d7357f345c66dd3cfb0a9
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-13 10:55:49 -0400

    Add first program notes

--- Basics/FirstProgram/FirstProgram.java
+++ Basics/FirstProgram/FirstProgram.java
@@ -1,7 +1,16 @@
 package FirstProgram;
 
+// Functions don't exist on their own. They should always belong in a class,
+// a container for one or more related functions (think of supermarket section)
+// Packages --> Classes --> Methods
+
 public class FirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    // Method: A function that is part of a class
+    public static void main(String[] args) { 
+        // Access Modifier: a keyword used to define the scope/level of accesibility
+        // of classes, methods, and variables
+        System.out.println("Hello World");
     }
+
 }
