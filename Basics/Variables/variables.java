45008c956d7f64622cb209817a7ddc58407b2ebf
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-14 14:13:27 -0400

    Add data type notes and first program notes

---
+++ Basics/Variables/variables.java
@@ -0,0 +1,8 @@
+public class variables {
+    public static void main(String[] args) {
+        int myAge = 18;
+        String myName = "Mike"; 
+        System.out.println("My name is " + myName + " and I am " + myAge + " years old.");
+    }
+
+}
