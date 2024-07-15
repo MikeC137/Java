bbac32c3b5dbb400529c48718759c36990ba24d9
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-15 12:18:08 -0400

    Add primitve types notes and file

---
+++ Basics/Variables/primitivetypes.java
@@ -0,0 +1,29 @@
+public class primitivetypes {
+    public static void main(String[] args) {
+        // PRIMITVE TYPES
+
+// byte (1) - a number within the range of -128 to 127
+// byte myByte = 30;
+
+// short (2) - a number within the range of -32,768 to 32,767
+// short myShort = 5763; 
+
+// int (4) - the most common practice because it provides a large range (-2,147,483,648 to 2,147,483,647)
+// int myInt = 1_537_463;
+
+// long (8) - a number withing the range of -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+//long myLong = 7_347_422_222L;
+
+// float (4) - a decimal number with up to 7 decimal digits
+// float myFloat = 7.3F;
+
+// double (8) - a decimal number with up to 16 decimal digits
+//double myDouble = 7.3;
+
+// char (2) - any individual character in the Unicode standard 
+//char myChar = 'H';
+
+// Boolean (1) - True or False
+//boolean isEligibile = true;
+    }
+}
