57c325c152aea545900ea8266356c62073681a8f
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-17 10:45:00 -0400

    Add constants example and notes

---
+++ Basics/Arrays and Constants/constants.java
@@ -0,0 +1,10 @@
+public class constants{
+
+    public static void main(String[] args) {
+        // The "final" keyword prevents the value of a variable from changing. 
+        final float PI = 3.14F; // We use all capital letters to name constants
+        // PI will always be 3.14 and any attempt to change it will result in an error
+        System.out.println(PI);
+    }
+
+}
