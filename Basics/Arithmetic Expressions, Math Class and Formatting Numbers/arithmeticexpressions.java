be2fd927e01d85c5d36a688e043e4f9ce5ae2d24
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-17 11:33:15 -0400

    Create arithmetic expression file and add notes and code examples

---
+++ Basics/Arithmetic Expressions, Math Class and Formatting Numbers/arithmeticexpressions.java
@@ -0,0 +1,17 @@
+public class arithmeticexpressions{
+    public static void main(String[] args) {
+        int x = 1;
+        x++; // The operator can also be applied as a prefix (++x) in this case
+        System.out.println(x);
+        int y = x++; 
+        // The value of y will remain the same because the operator was applied as a postfix. X will increase
+        System.out.println(y);
+
+        // Augmented assignment operators
+        x += 2;
+        x -= 2;
+        x *= 2;
+        x /= 2;
+    }
+
+}
