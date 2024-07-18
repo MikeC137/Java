2c9aaa85935a349cdbfacf92eecea2cbf1e3b220
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-18 12:36:47 -0400

    Create casting file + add casting code examples

---
+++ Basics/Casting/casting.java
@@ -0,0 +1,20 @@
+public class casting{
+    public static void main(String[] args) {
+        // Implicit Casting - Automatic casting that happens when a value is converted to a bigger data type 
+        // byte > short > int > long > float > double
+        short x = 1; // a short is 2 bytes
+        int y = x+2; // an integer is 4 bytes
+        System.out.println(y);
+
+        // Explicit Casting
+        double d = 1.1;
+        int i = (int)d + 2;
+        System.out.println(i);
+
+        // Wrapper Classes
+        String s = "1";
+        int n = Integer.parseInt(s) + 2;
+
+
+    }
+}
