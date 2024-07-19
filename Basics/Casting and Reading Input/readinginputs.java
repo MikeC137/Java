5faa4803d55338d450ee3b75714ba9c5c0d79259
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-19 10:48:18 -0400

    Add Scanner class code

--- Basics/Casting and Reading Input/readinginputs.java
+++ Basics/Casting and Reading Input/readinginputs.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class readinginputs {
-    
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // System.in is used to work with the terminal window
+        System.out.print("Age: ");
+        byte age = scanner.nextByte();
+        System.out.println("You are " + age + " years old.");
+
+        scanner.nextLine();
+        
+        System.out.print("Name: ");
+        String name = scanner.nextLine().trim();
+        System.out.println("Your name is " + name);
+        }; 
+    }
