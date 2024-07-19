555d1afa6b0c4700cd85427074f87fccc0f98a96
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-19 11:47:53 -0400

    Add code for Principal and Annual Interest Rate

--- Basics/Mortgage Calculator Project/mortgagecalc.java
+++ Basics/Mortgage Calculator Project/mortgagecalc.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+import java.text.NumberFormat;
+
 public class mortgagecalc {
     public static void main(String[] args) {
-        
+        // Principal
+        NumberFormat currency = NumberFormat.getCurrencyInstance(); 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: ");
+        Double Principal = scanner.nextDouble();
+        // System.out.println(currency.format(Principal));
+
+        scanner.nextLine();
+
+        // Interest Rate
+        System.out.println("Annual Interest Rate: ");
+        Double interestRate = scanner.nextDouble() / 100;
+        // System.out.println(interestRate);
+
     }
 }
