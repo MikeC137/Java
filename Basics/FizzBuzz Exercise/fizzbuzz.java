242ba5210f78d706b024234083e03a1ec8c931e1
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-20 16:26:05 -0400

    Add FizzBuzz code

---
+++ Basics/FizzBuzz Exercise/fizzbuzz.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class fizzbuzz{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+        boolean isDivisibleToThree = (number % 3 == 0);
+        boolean isDivisibleToFive = (number % 5 == 0);
+
+        if (isDivisibleToThree && isDivisibleToFive)
+            System.out.println("FizzBuzz");
+        else if (isDivisibleToThree) 
+            System.out.println("Fizz");
+        else if (isDivisibleToFive) 
+            System.out.println("Buzz");
+        else 
+            System.out.println(number);
+
+    }
+} 
