866a3d72935b5b02a83ce7f628a8b8bb02938063
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-20 14:43:00 -0400

    Create ternaryop.java and add code example

---
+++ Basics/Operators and Conditional Stements/ternaryop.java
@@ -0,0 +1,7 @@
+public class ternaryop {
+    public static void main(String[] args) {
+        // Ternary operator - a conditional operator that takes 3 operands and evaluates an expression based on a condition.
+        int income = 120_000;
+        String className = income > 100_000 ? "First" : "Economy"; // Condition - if - else
+    }
+}
