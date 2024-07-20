900a5080293bcb45b485387a5e2177cfea356992
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-20 12:13:42 -0400

    Create comparisonoperators.java + add code example

---
+++ Basics/Operators and Conditional Stements/comparisonoperators.java
@@ -0,0 +1,14 @@
+public class comparisonoperators {
+    public static void main(String[] args) {
+        // Comparison operators are used to compare primitive values
+        int x = 1;
+        int y = 1;
+        //Boolean expressions - true or false
+        System.out.println(x == y); 
+        System.out.println(x != y);
+        System.out.println(x > y);
+        System.out.println(x >= y);
+        System.out.println(x < y);
+        System.out.println(x <= y);
+    }
+}
