3c822dfbfde9ac80c8a717b651b350a9a9581052
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-20 14:15:18 -0400

    Add simplifying conditional statement example and notes

--- Basics/Operators and Conditional Stements/ifstatements.java
+++ Basics/Operators and Conditional Stements/ifstatements.java
@@ -4,11 +4,17 @@ public class ifstatements {
         if (temp > 30) { // curly braces added to define a code block
             System.out.println("It's a hot day");
             System.out.println("Drink water");
-        } 
-        else if (temp > 20) 
+        } else if (temp > 20) {
             System.out.println("Beautiful day!");
-        else 
+        } else {
             System.out.println("Cold Day");
+        }
 
+        // Simplifying if statements
+        int income = 120_000;
+        boolean highIncome = (income > 100_000); 
+        // Wrap the expression in parentheses for readability
+        // Remove unecessary conditional statements
+        
     }
 }
