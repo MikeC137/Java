d2dab3b4c612adc532dc5e901ded651ea226ff4a
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-21 11:31:45 -0400

    Add for loops code example and notes

---
+++ Basics/Loops/for_loops.java
@@ -0,0 +1,10 @@
+public class for_loops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++)
+        System.out.println("Hello World!");  
+    }
+}
+// The control first looks at the counter variable and executes the statement (ex: i = 0). Then, it will move 
+// on and check whether the given condition (i < 5) is true and as long as the condition is true, 
+// the code within the loop will be executed. At the end of this iteration, the control will perform 
+// an operation on the counter variable such as incrementing or decrementing it. 
