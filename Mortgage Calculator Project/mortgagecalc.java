db414aff3efcd1afb3421b7c9af64505f40a7bc3
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-21 14:38:00 -0400

    Create PERCENT constant and simplify conditional statements

--- Mortgage Calculator Project/mortgagecalc.java
+++ Mortgage Calculator Project/mortgagecalc.java
@@ -4,6 +4,8 @@ import java.text.NumberFormat;
 public class mortgagecalc {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
         double principal = 0 ;
         double interestRate = 0;
         byte period = 0;
@@ -13,11 +15,10 @@ public class mortgagecalc {
             System.out.print("Principal ($1K - $1M): ");
             principal = scanner.nextDouble();
 
-            if (principal < 1_000 || principal > 1_000_000) {
-                System.out.println("Enter a number between 1,000 and 1,000,000.");
-                continue;
+            if (principal >= 1_000 && principal <= 1_000_000) {
+                 break;
             } else {
-                break;
+               System.out.println("Enter a number between 1,000 and 1,000,000.");
             }
         }   
 
@@ -26,12 +27,12 @@ public class mortgagecalc {
             System.out.print("Annual Interest Rate: ");
             interestRate = scanner.nextDouble();
 
-            if (interestRate < 0 || interestRate > 30) {
+            if (interestRate > 0 && interestRate <= 30) {
+                interestRate /= PERCENT;
+                break;
+            } else {
                 System.out.println("Enter a value greater than 0 and less than or equal to 30.");
                 continue;
-            } else {
-                interestRate /= 100;
-                break;
             }
         }
 
@@ -40,16 +41,15 @@ public class mortgagecalc {
             System.out.print("Period (Years): ");
             period = scanner.nextByte();
 
-            if (period < 1 || period > 30) {
-                System.out.println("Enter a value between 1 and 30");
-                continue;
-            } else {
+            if (period >= 1 && period <= 30) {
                 break;
+            } else {
+                System.out.println("Enter a value between 1 and 30");
             }
         }
 
         // Calculation 
-        final byte MONTHS_IN_YEAR = 12;
+
         NumberFormat currency = NumberFormat.getCurrencyInstance(); 
         double mortgageNumerator = principal * (interestRate / MONTHS_IN_YEAR) ;
         double mortgageDenominator = 1 - (Math.pow(( 1 + interestRate/MONTHS_IN_YEAR), -MONTHS_IN_YEAR * period));
