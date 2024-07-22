352fbec4eadcd74455b6f04c0883984b2c5342fc
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-22 10:30:29 -0400

    Initiate scanner object and number variables

---
+++ Projects/Basic Calculator/basic_calculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class basic_calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float firstNumber = 0 ;
+        float secondNumber = 0 ;
+        System.out.println("Welcome to the Basic Calculator!");
+
+        // First Number
+//         while (true) {
+//             System.out.print("Enter the first number: ");
+//             firstNumber = scanner.nextFloat();
+//             if firstNumber {
+
+//             }
+//         }
+    }
+}
