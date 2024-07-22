9da7344faf5fc5bf0bed5197af6d316347db0095
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-22 12:09:07 -0400

    Add code for first and second number

--- Projects/Basic Calculator/basic_calculator.java
+++ Projects/Basic Calculator/basic_calculator.java
@@ -2,17 +2,48 @@ import java.util.Scanner;
 public class basic_calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        float firstNumber = 0 ;
-        float secondNumber = 0 ;
+        String firstNumberString = "" ;
+        String secondNumberString = "" ;
+        float numOne = 0 ;
+        float numTwo = 0 ;
+        String operation = "" ;
         System.out.println("Welcome to the Basic Calculator!");
 
-        // First Number
-//         while (true) {
-//             System.out.print("Enter the first number: ");
-//             firstNumber = scanner.nextFloat();
-//             if firstNumber {
+        
+        while (true) {
+        
+            // First Number
+            while (true) {
+                System.out.print("Enter the first number: ");
+                firstNumberString = scanner.nextLine();
 
-//             }
-//         }
+                try {
+                    numOne = Float.parseFloat(firstNumberString);
+                    break;
+                }
+                catch (NumberFormatException nfe) {
+                    System.out.println("Please enter a number: ");
+                }
+            }
+
+            // Second Number
+            while (true) {
+                System.out.print("Enter the second number: ");
+                secondNumberString = scanner.nextLine();
+
+                try {
+                    numTwo = Float.parseFloat(secondNumberString);
+                    break;
+                }
+                catch (NumberFormatException nfe) {
+                    System.out.println("Please enter a number: ");
+                }
+            }
+            
+            // Operation
+            System.out.println("Choose an operation ( +, -, *, ? ): +");
+            
+
+        }
     }
 }
