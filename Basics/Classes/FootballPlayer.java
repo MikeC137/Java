4cc003b5388daf5385d78bd548b244eb1bbb3d0f
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-22 23:06:40 -0400

    Add accessors and mutators

--- Basics/Classes/FootballPlayer.java
+++ Basics/Classes/FootballPlayer.java
@@ -1,12 +1,12 @@
 public class FootballPlayer {
     // Fields
-    String firstName;
-    String lastName;
-    int age;
-    String nationality;
-    String position;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String nationality;
+    private String position;
 
-    FootballPlayer() {
+    public FootballPlayer() {
         firstName = "";
         lastName = "";
         age = 1;
@@ -23,15 +23,59 @@ public class FootballPlayer {
     }
 
     // Methods
-    void run() {
+
+    // Mutators/Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    // Accessors/Getters
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void run() {
         System.out.println("The football player is running");
     } 
 
-    void shoot() {
+    public void shoot() {
         System.out.println("The football player is shooting");
     }
 
-    void pass() {
+    public void pass() {
         System.out.println("The football player is passing");
     }
 }
--- Basics/Classes/Main.java
+++ Basics/Classes/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         FootballPlayer fp = new FootballPlayer("Mike", "Chatzi", 18, "Greek", "CB");
-        System.out.println(fp.firstName);
+        System.out.println(fp.getFirstName());
     }
 }
