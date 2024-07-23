757d4e19aad310c5cc011515f2bb5f7c4dfca969
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-23 12:48:11 -0400

    Add code for Vowel Counter Exercise

---
+++ Exercises/Vowel Counter/VowelCounter.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class VowelCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+
+
+        System.out.print("Enter a string: ");
+        String userString = scanner.nextLine().toLowerCase();
+
+        for (int i = 0; i < userString.length(); i++)
+            if (userString.charAt(i) == 'a' || userString.charAt(i) == 'e' || userString.charAt(i) == 'i' || userString.charAt(i) == 'o' || userString.charAt(i) == 'u' ) {
+                counter += 1;
+            } 
+
+
+        // for (char vowel : vowels)
+        //     if (userString.contains(Character.toString(vowel))) {
+        //         counter += 1;
+        //         System.out.println(vowel);
+        //     }
+        
+        System.out.println("The number of vowels in the string is: " + counter);
+    }
+}
