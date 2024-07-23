fd4d658b75f864a865a8cd087958796d04a36f7f
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-23 17:11:57 -0400

    Optimize code in palindrome checker

--- Exercises/Palindrome Checker/PalindromeChecker.java
+++ Exercises/Palindrome Checker/PalindromeChecker.java
@@ -4,18 +4,17 @@ public class PalindromeChecker {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String userString = scanner.next().toLowerCase().replaceAll("[^a-zA-Z0-9]","");
-        String reverseString = "";
+        String userString = scanner.nextLine().toLowerCase().replaceAll("[^a-zA-Z0-9]","");
         
-        for (int i = userString.length() - 1; i >= 0 ; i--) {
-            reverseString += userString.charAt(i);
-        } 
+        StringBuilder reverseStringBuilder = new StringBuilder(userString);
+        String reverseString = reverseStringBuilder.reverse().toString();
 
         if (reverseString.equals(userString)){
             System.out.println("The string is a palindrome");
-
         } else {
             System.out.println("The string is not a palindrome");
         }
+        
+        scanner.close();
     }
 }
