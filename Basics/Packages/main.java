24b1712844c933085a241f2c9c5d95496b6a690c
MikeC137 <michalischatzinikolas13@gmail.com>
2024-07-29 13:54:11 -0400

    Create FirstPackage and add notes

---
+++ Basics/Packages/main.java
@@ -0,0 +1,7 @@
+import FirstPackage.HelloWorld; // Calls a specific class from a package
+// OR
+import FirstPackage.*; // Calls all classes from a package
+
+public class main {
+    HelloWorld helloWorld = new HelloWorld();
+}
